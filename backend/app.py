419806a3dd4759f9c680bd5d913b6d39d96ae4cf
Tharun N Nanju <tharunnnanju@gmail.com>
2025-04-08 12:32:14 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import socket
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests', ['container'])
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.labels(container=socket.gethostname()).inc()
+    return f"Hello from container: {socket.gethostname()}"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
